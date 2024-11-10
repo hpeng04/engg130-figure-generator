2f0dbff540a9f260f1730cf234d984bd9e014d6f
hpeng04 <144637938+hpeng04@users.noreply.github.com>
2024-11-09 17:54:33 -0700

    create q10 outline

---
+++ q10.py
@@ -0,0 +1,13 @@
+def generate_figure(length_OA, length_AB, length_BC):
+    # Draw bar
+
+    # Draw force distribution
+
+    # Draw triangle supports
+
+    # Label distances
+    
+    pass
+
+if __name__ == "__main__":
+    pass
